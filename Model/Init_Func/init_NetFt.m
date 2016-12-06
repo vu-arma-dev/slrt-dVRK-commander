b16b70de370185b67d20759c5d1649fa761196ae
Long Wang <wanglong06@gmail.com>
2016-12-06 16:34:27 -0600

    force sensor data tested using sawATIForceSensorExample to read

--- Model/Init_Func/init_NetFt.m
+++ Model/Init_Func/init_NetFt.m
@@ -1,6 +1,6 @@
 %%  main init for ATI_NetFt
 %   Do NOT use this together with "init_psm_cmd"
-dt = 0.0005;
+dt = 0.001;
 socketSentFtMsgTypes = cell(1,9);
 for i = 1:3
     socketSentFtMsgTypes{i} = 'uint32';
@@ -12,4 +12,4 @@ ATI_NetFt_reading_Gain = 1000000;
 % the gain value of "ATI_NetFt_reading_Gain" is set by the ATI Net F/T box
 % and it is reflected in the parsing function in mtsATINetFTSensor.cpp - 
 % https://github.com/jhu-saw/sawATIForceSensor/blob/df38da0fd6e7176dad737502f219dbaf46491c85/code/mtsATINetFTSensor.cpp#L159
-ATI_NetFt_port = 49152;
+% ATI_NetFt_port = 49152;
