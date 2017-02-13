fc5fdf53f8288f397f90129c3172787f56d42e33
irep_dev <rashid.m.yasin@vanderbilt.edu>
2017-02-13 15:33:30 -0600

    Added ability not to reset path when running force sensor

--- Scripts/Main_ATI_NetFT.m
+++ Scripts/Main_ATI_NetFT.m
@@ -3,26 +3,40 @@ function ATI_NetFT = Main_ATI_NetFT(varargin)
 %   By Long Wang
 %   This func starts the xPC application that does the same protocol as ATI
 %   Net FT box
+% Properties:
+% 'FT Type': nano17
+%           gamma
+%           NOT IMPLEMENTED: nano43
+% 'Path': 'Keep' - do not add to path or reset path
+%         'Reset' - add to and reset path
+
 %%  parse the input:
 FT_Type= 'nano17';
+Path_Type='Reset';
 if numel(varargin)
     for i = 1:2:numel(varargin)
         propertyName = varargin{i};
         propertyValue = varargin{i+1};
         if strcmp(propertyName,'FT Type')
             FT_Type = propertyValue;
+        elseif strcmp(propertyName,'Path')
+            Path_Type= propertyValue;
         end
     end
 end
-%%  Folder path
+
 Model_path = getenv('PSMCMD');
-ECL_path = getenv('ECLDIR');
-fprintf('Setting up all the directories ..')
-working_dir = fileparts(Model_path);
-restoredefaultpath;
-addpath(genpath(working_dir),...
-    genpath(ECL_path));
-fprintf('..[ok]\n');
+
+if strcmp(Path_Type,'Reset')
+    %%  Folder path
+    ECL_path = getenv('ECLDIR');
+    fprintf('Setting up all the directories ..')
+    working_dir = fileparts(Model_path);
+    restoredefaultpath;
+    addpath(genpath(working_dir),...
+        genpath(ECL_path));
+    fprintf('..[ok]\n');
+end
 %%  xPC Model information
 switch FT_Type
     case 'gamma'
